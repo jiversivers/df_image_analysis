12cf3b87e5ebf8b748a841cfe66a4cb82001b10c
jiversivers <jdivers@uark.edu>
2024-12-10 16:42:05 -0600

    started organizing multi parameter set simulation. starting form so2 and thb is going to actually be overkill, so this can be scaled back. these can be calculated after fitting

--- monte_carlo_simulator.py
+++ monte_carlo_simulator.py
@@ -1,30 +1,43 @@
 import my_modules.monte_carlo.monte_carlo as mc
+import numpy as np
+
+from my_modules.image_processing.quantification import get_coefficients, calculate_mus
 
 
 def main():
-    # make the system
-    di_water = mc.OpticalMedium(1.333, 0, 0.0008, 0, type='di water')
-    tissue = mc.OpticalMedium(1.55, 90, 10, 0.75, type='tissue')
-    system = mc.System(di_water, 0.1, tissue, 100)
-    T, R, A = 3 * [0]
-    paths = []
-    j = 0
-    for i in range(100):
-        photon = mc.Photon(650, system=system)
-        while not photon.is_terminated:
-            photon.move()
-            if photon.is_terminated:
-                break
-            photon.absorb()
-            if photon.is_terminated:
-                break
-            photon.scatter()
-            j += 1
-        paths.append(photon.location_history)
-        T += photon.T
-        R += photon.R
-        A += photon.A
-    print(T, R, A)
+    # Init parameter sets
+    sO2 = np.linspace(0, 1, num=100)  # %
+    tHb = np.linspace(10, 20, num=100)  # mg/mL
+    wavelengths = np.arange(400, 725, 5)  # nm
+    G = np.linspace(0.5, 1, num=10)
+
+    # Make water medium
+    di_water = mc.OpticalMedium(n=1, mu_s=0.003, mu_a=0, g=0, type='di water')
+
+    for so2 in sO2:
+        for thb in tHb:
+            # Determine coefficients at given parameter set
+            wavelengths, hbo2, hb = get_coefficients(wavelengths, db_file='hbo2_hb.db')
+            mu_s, mu_a = calculate_mus(a=1, b=1, chb=thb, alpha=so2, wavelength=wavelengths, hb=hb, hbo2=hbo2)
+            for wavelength in wavelengths:
+                for g in G:
+                    for s, a in zip(mu_s, mu_a):
+                        # Make the system
+                        tissue = mc.OpticalMedium(n=1.4, mu_s=s, mu_a=a, g=g, type='tissue')
+                        system = mc.System(di_water, 0.01, tissue, 0.05, surrounding_n=1)
+                        T, R, A = 3 * [0]
+                        n = 50000
+                        for i in range(n):
+                            photon = mc.Photon(650, system=system)
+                            while not photon.is_terminated:
+                                photon.absorb()
+                                photon.move()
+                                photon.scatter()
+                            T += photon.T
+                            R += photon.R
+                            A += photon.A
+                            simulation_parameters=(sO2, tHb, wavelength, g)
+                            simulation_results = (T, R, A)
 
 if __name__ == '__main__':
     main()

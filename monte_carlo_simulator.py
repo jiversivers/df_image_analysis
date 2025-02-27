025e7c3bbec7815a3c3668e104df1496dbd9e99c
jiversivers <jdivers@uark.edu>
2025-02-27 12:39:15 -0600

    correcting dtype issues causing BLOBs

--- monte_carlo_simulator.py
+++ monte_carlo_simulator.py
@@ -18,9 +18,9 @@ print(f"Using {np.__name__}")
 
 def main():
     # Init parameter sets
-    mu_s_array = np.linspace(0, 100, 40)
-    mu_a_array = np.linspace(1, 101, 40)
-    g_array = [0.85]
+    mu_s_array = np.arange(0, 101, 5)
+    mu_a_array = np.arange(1, 102, 5)
+    g_array = [0.8, 0.85, 0.9]
     d_array = [float('inf')]  # Fix as semi-infinite
     n = 50000
     tissue_n = 1.33
@@ -42,7 +42,8 @@ def main():
         transmission REAL,
         reflectance REAL,
         absorption REAL,
-        simulation_id INTEGER
+        simulation_id INTEGER,
+        forced BOOLEAN DEFAULT FALSE
     )""")
 
     c.execute("""
@@ -54,13 +55,14 @@ def main():
     water_mu_s REAL NOT NULL,
     water_mu_a REAL NOT NULL,
     tissue_n REAL NOT NULL,
-    surroundings_n REAL NOT NULL)""")
+    surroundings_n REAL NOT NULL,
+    recursive BOOLEAN DEFAULT FALSE)""")
 
     c.execute(f"""
     INSERT INTO mclut_simulations (
     photon_count, dimensionality, water_n, water_mu_s, water_mu_a, tissue_n, surroundings_n
     ) VALUES (?, ?, ?, ?, ?, ?, ?)""", (
-        n, 1, di_water.n, di_water.mu_s, di_water.mu_a, tissue_n, surroundings_n
+        n, 1, float(di_water.n), float(di_water.mu_s), float(di_water.mu_a), float(tissue_n), float(surroundings_n)
     ))
     conn.commit()
     simulation_id = c.lastrowid
@@ -71,13 +73,26 @@ def main():
         # Make the system
         tissue = mc.OpticalMedium(n=tissue_n, mu_s=mu_s, mu_a=mu_a, g=g, type='tissue')
         system = mc.System(di_water, 0.01, tissue, d, surrounding_n=surroundings_n)
-        T, R, A, = mc.simulate(system, n=10000)
+        T, R, A = 3 * [0]
+
+        for i in range(n):
+            photon = mc.Photon(650, system=system, recurse=True)
+            while not photon.is_terminated:
+                photon.absorb()
+                photon.move()
+                photon.scatter()
+            T += photon.T
+            R += photon.R
+            A += photon.A
+        print(f'\nmu_s: {mu_s}, mu_a: {mu_a}, g: {g}, d: {d}\n'
+              f'T: {100 * T / n}%, R: {100 * R / n}%, A: {100 * A / n}\n%')
+
         # Add results to db
         c.execute(f"""
                     INSERT INTO mclut (
-                    mu_s, mu_a, g, depth, transmission, reflectance, absorption, simulation_id
-                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", (
-            mu_s, mu_a, g, d, T / n, R / n, A / n, simulation_id
+                    mu_s, mu_a, g, depth, transmission, reflectance, absorption, simulation_id, forced
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, false)""", (
+            float(mu_s), float(mu_a), float(g), float(d), float(T / n), float(R / n), float(A / n), simulation_id
         ))
     conn.commit()
 

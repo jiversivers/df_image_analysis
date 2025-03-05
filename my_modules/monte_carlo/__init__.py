36e7fbb84f40b88e5a9374c003b71cf78bb70396
jiversivers <jdivers@uark.edu>
2025-03-05 17:40:26 -0600

    improved lookup functionality with RegularGrid interp/extrap

--- my_modules/monte_carlo/__init__.py
+++ my_modules/monte_carlo/__init__.py
@@ -1,26 +1,33 @@
 import sqlite3
-from scipy.interpolate import CubicSpline
+
+import numpy as np
+from scipy.interpolate import CubicSpline, RegularGridInterpolator
 from .monte_carlo import Photon, OpticalMedium, System
 
 # Setup default database and MCLUT version
-conn = sqlite3.connect('databases/hsdfm_data.db')
+conn = sqlite3.connect(r'C:\Users\jdivers\PycharmProjects\df_image_analysis\databases\hsdfm_data.db')
 c = conn.cursor()
 c.execute("SELECT max(id) FROM mclut_simulations")
 simulation_id = c.fetchone()[0]
 
 
-def lookup(mu_s, mu_a, g, depth, conn=conn, simulation_id=None, force_simulate=True, recurse=True):
+def lookup(mu_s, mu_a, g, depth,
+           conn=conn, simulation_id=None, save_sim=True, force_simulate=False, skip_simulation=False):
+    assert (
+        not (force_simulate and skip_simulation),
+        'It is ambiguous to set both skip_simulation and force_simulate to TRUE.')
+
+    # If simulation_id is NONE, default to most recent
     if simulation_id is None:
-        # If simulation_id is NONE, default to most recent, and then search the whole db
         c.execute("SELECT * FROM mclut_simulations ORDER BY id DESC LIMIT 1")
         simulation_id = c.fetchone()[0]
 
     # Fetch available parameters within the simulation_id
     c.execute("""
-        SELECT mu_s, mu_a, g, depth, reflectance 
-        FROM mclut
-        WHERE simulation_id = ?
-    """, (simulation_id,))
+            SELECT mu_s, mu_a, g, depth, reflectance 
+            FROM mclut
+            WHERE simulation_id = ?
+        """, (simulation_id,))
 
     rows = c.fetchall()
 
@@ -34,30 +41,52 @@ def lookup(mu_s, mu_a, g, depth, conn=conn, simulation_id=None, force_simulate=T
     if exact_match:
         return exact_match[-1]  # Return the result value for the exact match
 
+    # Removed forced simulations from interpolation data
+    c.execute("""
+            SELECT mu_s, mu_a, g, depth, reflectance 
+            FROM mclut
+            WHERE simulation_id = ? AND forced = False
+        """, (simulation_id,))
+    rows = c.fetchall()
+
     # If no exact match, check if parameters are within the bounds for interpolation
     mu_s_vals, mu_a_vals, g_vals, depth_vals, ref_vals = zip(*rows)
 
     # Find nearest bounds for interpolation. Here we assume cubic spline interpolation for all parameters.
-    if not force_simulate and (min(mu_s_vals) <= mu_s <= max(mu_s_vals) and
-                               min(mu_a_vals) <= mu_a <= max(mu_a_vals) and
-                               min(g_vals) <= g <= max(g_vals) and
-                               min(depth_vals) <= depth <= max(depth_vals)):
-        # Interpolation setup
-        spline = CubicSpline(mu_s_vals, ref_vals)
-        interpolated_value = spline(mu_s)
-        return interpolated_value
+
+    if not force_simulate:
+        # Get data into a regular grid
+        x = np.unique(mu_s_vals)
+        y = np.unique(mu_a_vals)
+        z = np.asarray(ref_vals).reshape(len(x), len(y))
+
+        # Interpolation
+        spline = RegularGridInterpolator((x, y), z, method='cubic', bounds_error=False, fill_value=None)
+        interpolated_ref = spline((mu_s, mu_a))
+        return interpolated_ref.item()
 
     # If query is out of bounds or if force_simulate is True, simulate the result
-    else:
-        # Here, simulate a new result. This could be a complex function or simulation based on mu_s, mu_a, g, depth
-        T, R, A = simulate(mu_s, mu_a, g, depth, recurse=recurse)
+    elif not skip_simulation:
+        print(f'Simulating for mu_s={mu_s}, mu_a={mu_a}, g={g}, depth={depth}')
+        # Get matching simulation parameters
+        c.execute("""SELECT photon_count, water_n, water_mu_s, water_mu_a, tissue_n, surroundings_n, recursive
+            FROM mclut_simulations WHERE id = ?""", (simulation_id,))
+        params = c.fetchone()
+
+        # Re-create system
+        di_water = OpticalMedium(n=params[1], mu_s=params[2], mu_a=params[3], g=0, type='di_water')
+        tissue = OpticalMedium(n=params[4], mu_s=mu_s, mu_a=mu_a, g=g, type='tissue')
+        system = System(di_water, 0.01, tissue, depth, surrounding_n=params[5])
+
+        T, R, A = simulate(system, n=params[0], recurse=params[6])
 
         # Insert the new simulation into the database
-        c.execute("""
-            INSERT INTO mclut (simulation_id, mu_s, mu_a, g, depth, transmission, reflectance, absorption, forced)
-            VALUES (?, ?, ?, ?, ?, ?)
-        """, (simulation_id, mu_s, mu_a, g, depth, T, R, A, True))
-        conn.commit()
+        if save_sim:
+            c.execute("""
+                    INSERT INTO mclut (simulation_id, mu_s, mu_a, g, depth, transmission, reflectance, absorption, forced)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """, (simulation_id, mu_s, mu_a, g, depth, T, R, A, True))
+            conn.commit()
 
         return R
 
@@ -65,9 +94,9 @@ def lookup(mu_s, mu_a, g, depth, conn=conn, simulation_id=None, force_simulate=T
 def simulate(system, n=50000, photon=None, recurse=True):
     T, R, A = 3 * [0]
     for i in range(n):
-        photon = Photon(650, system=system, recurse=recurse) if photon is None else photon
-        photon.simulate()
-        T += photon.T
-        R += photon.R
-        A += photon.A
+        current_photon = Photon(650, system=system, recurse=recurse) if photon is None else photon.copy()
+        current_photon.simulate()
+        T += current_photon.T
+        R += current_photon.R
+        A += current_photon.A
     return T / n, R / n, A / n

e1ead0bb637d64f714ab5dedb99d223178f7fff1
jiversivers <jdivers@uark.edu>
2025-03-21 13:14:14 -0500

    cleaning dir structure for packaging

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .monte_carlo.optics import Photon, Medium, System, Illumination, Detector
-from .monte_carlo.utils import lookup, simulate
-from .monte_carlo import hardware
--- tests/images_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestHSDFMImage(unittest.TestCase):
-    def setUp(self):
-        pass
-    def test_map_vasculature(self):
-        # Test filter response with simple knonw case (ones array or array with perfect corellation maybe)
-        pass
